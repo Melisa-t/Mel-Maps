6198f37233c1dd1a6ac221684183afe595410ab0
Melisa Lachev <melisatormac55@gmail.com>
2025-05-28 13:06:39 +0300

    created marker on click

--- script.js
+++ script.js
@@ -24,10 +24,23 @@ navigator.geolocation?.getCurrentPosition(
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup("Gotcha!")
-      .openPopup();
+    map.on(`click`, function (mapEvent) {
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnEscapeKey: false,
+            closeButton: false,
+            closeOnClick: false,
+            className: `running-popup`, 
+          })
+        ).setPopupContent(`Running workout on May 28!`)
+        .openPopup();
+    });
   },
   function () {
     alert(`Couldn't get your location! Allow me!`);
